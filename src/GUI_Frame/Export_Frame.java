24331f4fba14ccbdb1477bd635069663e676fbd9
pontida123456789 <pontida.sen@gmail.com>
2024-06-04 10:56:25 +0700

    revised hide on close in Export_Frame

--- src/GUI_Frame/Export_Frame.java
+++ src/GUI_Frame/Export_Frame.java
@@ -70,7 +70,7 @@ public class Export_Frame extends JFrame {
 	 * Create the frame.
 	 */
 	public Export_Frame() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		setBounds(100, 100, 450, 237);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
